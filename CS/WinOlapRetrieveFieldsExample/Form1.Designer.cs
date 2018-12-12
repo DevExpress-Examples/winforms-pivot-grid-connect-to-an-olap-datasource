ba3bfe986c489efb4d029f53582760c9a2137759
BrianDX <boris.zaitsev@devexpress.com>
2018-12-12 14:12:21 +0300

    Update example and description to illustrate how to use the OLAPExpression field property,

--- CS/WinOlapRetrieveFieldsExample/Form1.Designer.cs
+++ CS/WinOlapRetrieveFieldsExample/Form1.Designer.cs
@@ -28,24 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DevExpress.XtraPivotGrid.PivotGridGroup pivotGridGroup1 = new DevExpress.XtraPivotGrid.PivotGridGroup();
+            DevExpress.XtraPivotGrid.PivotGridGroup pivotGridGroup2 = new DevExpress.XtraPivotGrid.PivotGridGroup();
             this.pivotGridControl1 = new DevExpress.XtraPivotGrid.PivotGridControl();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.btnRetrieveFields = new DevExpress.XtraEditors.SimpleButton();
+            this.btnCreateUnboundFields = new DevExpress.XtraEditors.SimpleButton();
             ((System.ComponentModel.ISupportInitialize)(this.pivotGridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pivotGridControl1
             // 
             this.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            pivotGridGroup1.Caption = "Date.Fiscal";
-            pivotGridGroup1.Hierarchy = "[Date].[Fiscal]";
+            pivotGridGroup2.Caption = "Date.Fiscal";
+            pivotGridGroup2.Hierarchy = "[Date].[Fiscal]";
             this.pivotGridControl1.Groups.AddRange(new DevExpress.XtraPivotGrid.PivotGridGroup[] {
-            pivotGridGroup1});
-            this.pivotGridControl1.Location = new System.Drawing.Point(0, 0);
+            pivotGridGroup2});
+            this.pivotGridControl1.Location = new System.Drawing.Point(0, 52);
             this.pivotGridControl1.Name = "pivotGridControl1";
             this.pivotGridControl1.OLAPConnectionString = "";
             this.pivotGridControl1.OLAPDataProvider = DevExpress.XtraPivotGrid.OLAPDataProvider.Adomd;
-            this.pivotGridControl1.Size = new System.Drawing.Size(755, 439);
+            this.pivotGridControl1.Size = new System.Drawing.Size(755, 387);
             this.pivotGridControl1.TabIndex = 0;
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.btnCreateUnboundFields);
+            this.panelControl1.Controls.Add(this.btnRetrieveFields);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(755, 52);
+            this.panelControl1.TabIndex = 1;
+            // 
+            // btnRetrieveFields
+            // 
+            this.btnRetrieveFields.AccessibleDescription = "            pivotGridControl1.Fields[\"[Customer].[Country].[Country]\"]";
+            this.btnRetrieveFields.Location = new System.Drawing.Point(30, 12);
+            this.btnRetrieveFields.Name = "btnRetrieveFields";
+            this.btnRetrieveFields.Size = new System.Drawing.Size(107, 23);
+            this.btnRetrieveFields.TabIndex = 0;
+            this.btnRetrieveFields.Text = "Retrieve Fields";
+            this.btnRetrieveFields.Click += new System.EventHandler(this.btnRetrieveFields_Click);
+            // 
+            // btnCreateUnboundFields
+            // 
+            this.btnCreateUnboundFields.AccessibleDescription = "";
+            this.btnCreateUnboundFields.Location = new System.Drawing.Point(174, 12);
+            this.btnCreateUnboundFields.Name = "btnCreateUnboundFields";
+            this.btnCreateUnboundFields.Size = new System.Drawing.Size(107, 23);
+            this.btnCreateUnboundFields.TabIndex = 1;
+            this.btnCreateUnboundFields.Text = "Create Fields";
+            this.btnCreateUnboundFields.Click += new System.EventHandler(this.btnCreateFields_Click);
             // 
             // Form1
             // 
@@ -53,9 +88,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(755, 439);
             this.Controls.Add(this.pivotGridControl1);
+            this.Controls.Add(this.panelControl1);
             this.Name = "Form1";
             this.Text = "WinForms Pivot Grid OLAP Example";
             ((System.ComponentModel.ISupportInitialize)(this.pivotGridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -63,6 +101,9 @@
         #endregion
 
         private DevExpress.XtraPivotGrid.PivotGridControl pivotGridControl1;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.SimpleButton btnCreateUnboundFields;
+        private DevExpress.XtraEditors.SimpleButton btnRetrieveFields;
     }
 }
 
--- CS/WinOlapRetrieveFieldsExample/Form1.cs
+++ CS/WinOlapRetrieveFieldsExample/Form1.cs
@@ -1,6 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using DevExpress.XtraPivotGrid;
 using DevExpress.XtraPivotGrid.Customization;
-using DevExpress.XtraPivotGrid;
 
 namespace WinOlapRetrieveFieldsExample
 {
@@ -9,7 +8,7 @@ namespace WinOlapRetrieveFieldsExample
         public Form1()
         {
             InitializeComponent();
-            // Specifies the OLAP connection settings.
+            // Specify the OLAP connection settings.
             pivotGridControl1.OLAPDataProvider = OLAPDataProvider.Adomd;
             pivotGridControl1.OLAPConnectionString =
                 @"Provider=MSOLAP;
@@ -18,26 +17,63 @@ namespace WinOlapRetrieveFieldsExample
                 Cube name=Adventure Works;
                 Query Timeout=100;";
 
-            // Retrieves fields.
+            // Set the Customization Forms style.
+            pivotGridControl1.OptionsCustomization.CustomizationFormStyle = CustomizationFormStyle.Excel2007;
+            // Invoke the Customization Form.
+            pivotGridControl1.FieldsCustomization();
+        }
+
+        private void btnRetrieveFields_Click(object sender, System.EventArgs e)
+        {
+            // Retrieve fields.
             pivotGridControl1.RetrieveFields(PivotArea.ColumnArea, false);
 
-            // Adds some fields from the Field List to the specified area to create a report.
+            // Add fields from the Field List to the specified area to create a report.
+            pivotGridControl1.BeginUpdate();
             pivotGridControl1.Fields["[Customer].[Country].[Country]"].Area = PivotArea.RowArea;
+            pivotGridControl1.Fields["[Customer].[Country].[Country]"].OLAPDimensionCaption = "TEST";
             pivotGridControl1.Fields["[Customer].[Country].[Country]"].Visible = true;
             pivotGridControl1.Fields["[Customer].[City].[City]"].Area = PivotArea.RowArea;
             pivotGridControl1.Fields["[Customer].[City].[City]"].Visible = true;
             pivotGridControl1.Fields["[Date].[Fiscal].[Fiscal Year]"].Area = PivotArea.ColumnArea;
             pivotGridControl1.Fields["[Date].[Fiscal].[Fiscal Year]"].Visible = true;
             pivotGridControl1.Fields["[Measures].[Internet Sales Amount]"].Visible = true;
+            pivotGridControl1.EndUpdate();
 
-            PivotGridField dimensionField = new PivotGridField() { Caption = "Cleared Amount", Area = PivotArea.DataArea };
-            dimensionField.OLAPExpression = "[Measures].[Internet Sales Amount] * 0.87";
-            pivotGridControl1.Fields.Add(dimensionField);
+            // Resize columns automatically.
+            pivotGridControl1.BestFit();
 
-            // Sets the Customization Forms style to Excel2007 with additional capabilities.
-            pivotGridControl1.OptionsCustomization.CustomizationFormStyle = CustomizationFormStyle.Excel2007;
+            // Invoke the Customization Form.
+            pivotGridControl1.FieldsCustomization();
+        }
 
-            // Invokes the Customization Form.
+        private void btnCreateFields_Click(object sender, System.EventArgs e)
+        {
+            pivotGridControl1.BeginUpdate();
+            pivotGridControl1.Fields.Clear();
+
+            // Create a field, specify a query expression to obtain data and a caption to display it in the Customization form.
+            PivotGridFieldBase fieldCountry = pivotGridControl1.Fields.Add("Country", PivotArea.RowArea);
+            fieldCountry.OLAPExpression = "[Customer].[Country].[Country]";
+            fieldCountry.OLAPDimensionCaption = "Location";
+
+            PivotGridFieldBase fieldCity = pivotGridControl1.Fields.Add("City", PivotArea.RowArea);
+            fieldCity.OLAPExpression = "[Customer].[City].[City]";
+            fieldCity.OLAPDimensionCaption = "Location";
+
+            PivotGridField measureField = new PivotGridField() { Caption = "Cleared Amount", Area = PivotArea.DataArea };
+            measureField.OLAPExpression = "[Measures].[Internet Sales Amount] * 0.87";
+            measureField.OLAPDimensionCaption = "Sales";
+            pivotGridControl1.Fields.Add(measureField);
+
+            PivotGridFieldBase fieldTop10 = pivotGridControl1.Fields.Add("Top10", PivotArea.ColumnArea);
+            fieldTop10.OLAPExpression = "TOPCOUNT([Date].[Date].[Date].MEMBERS, 10, [Measures].[Internet Sales Amount])";
+            fieldTop10.OLAPDimensionCaption = "Top";
+            fieldTop10.Visible = false;;
+
+            pivotGridControl1.EndUpdate();
+
+            // Invoke the Customization Form.
             pivotGridControl1.FieldsCustomization();
         }
     }
