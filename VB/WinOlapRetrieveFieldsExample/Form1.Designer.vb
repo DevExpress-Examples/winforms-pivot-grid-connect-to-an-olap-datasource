23d46d5df8a01bca0745db88abfe9cce4686a77a
DevExpressExampleBot <fakeEmail>
2022-06-30 10:07:03 +0400

    Source auto update [skip ci]

--- VB/WinOlapRetrieveFieldsExample/Form1.Designer.vb
+++ VB/WinOlapRetrieveFieldsExample/Form1.Designer.vb
@@ -1,53 +1,51 @@
-﻿Namespace WinOlapRetrieveFieldsExample
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace WinOlapRetrieveFieldsExample
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-            Dim PivotGridGroup1 As DevExpress.XtraPivotGrid.PivotGridGroup = New DevExpress.XtraPivotGrid.PivotGridGroup()
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
+
+            MyBase.Dispose(disposing)
+        End Sub
+
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
             Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
             Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
             Me.btnCreateUnboundFields = New DevExpress.XtraEditors.SimpleButton()
             Me.btnRetrieveFields = New DevExpress.XtraEditors.SimpleButton()
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.panelControl1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.panelControl1.SuspendLayout()
             Me.SuspendLayout()
-            '
-            'pivotGridControl1
-            '
+            ' 
+            ' pivotGridControl1
+            ' 
             Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-            PivotGridGroup1.Caption = "Date.Fiscal"
-            PivotGridGroup1.Hierarchy = "[Date].[Fiscal]"
-            Me.pivotGridControl1.Groups.AddRange(New DevExpress.XtraPivotGrid.PivotGridGroup() {PivotGridGroup1})
             Me.pivotGridControl1.Location = New System.Drawing.Point(0, 52)
             Me.pivotGridControl1.Name = "pivotGridControl1"
             Me.pivotGridControl1.OLAPConnectionString = ""
             Me.pivotGridControl1.OLAPDataProvider = DevExpress.XtraPivotGrid.OLAPDataProvider.Adomd
             Me.pivotGridControl1.Size = New System.Drawing.Size(755, 387)
             Me.pivotGridControl1.TabIndex = 0
-            '
-            'panelControl1
-            '
+            ' 
+            ' panelControl1
+            ' 
             Me.panelControl1.Controls.Add(Me.btnCreateUnboundFields)
             Me.panelControl1.Controls.Add(Me.btnRetrieveFields)
             Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Top
@@ -55,47 +53,49 @@
             Me.panelControl1.Name = "panelControl1"
             Me.panelControl1.Size = New System.Drawing.Size(755, 52)
             Me.panelControl1.TabIndex = 1
-            '
-            'btnCreateUnboundFields
-            '
+            ' 
+            ' btnCreateUnboundFields
+            ' 
             Me.btnCreateUnboundFields.AccessibleDescription = ""
             Me.btnCreateUnboundFields.Location = New System.Drawing.Point(174, 12)
             Me.btnCreateUnboundFields.Name = "btnCreateUnboundFields"
             Me.btnCreateUnboundFields.Size = New System.Drawing.Size(107, 23)
             Me.btnCreateUnboundFields.TabIndex = 1
             Me.btnCreateUnboundFields.Text = "Create Fields"
-            '
-            'btnRetrieveFields
-            '
+            AddHandler Me.btnCreateUnboundFields.Click, New System.EventHandler(AddressOf Me.btnCreateFields_Click)
+            ' 
+            ' btnRetrieveFields
+            ' 
             Me.btnRetrieveFields.AccessibleDescription = "            pivotGridControl1.Fields[""[Customer].[Country].[Country]""]"
             Me.btnRetrieveFields.Location = New System.Drawing.Point(30, 12)
             Me.btnRetrieveFields.Name = "btnRetrieveFields"
             Me.btnRetrieveFields.Size = New System.Drawing.Size(107, 23)
             Me.btnRetrieveFields.TabIndex = 0
             Me.btnRetrieveFields.Text = "Retrieve Fields"
-            '
-            'Form1
-            '
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            AddHandler Me.btnRetrieveFields.Click, New System.EventHandler(AddressOf Me.btnRetrieveFields_Click)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
             Me.ClientSize = New System.Drawing.Size(755, 439)
             Me.Controls.Add(Me.pivotGridControl1)
             Me.Controls.Add(Me.panelControl1)
             Me.Name = "Form1"
             Me.Text = "WinForms Pivot Grid OLAP Example"
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.panelControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.panelControl1.ResumeLayout(False)
             Me.ResumeLayout(False)
-
         End Sub
 
 #End Region
-
         Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
-		Private panelControl1 As DevExpress.XtraEditors.PanelControl
-		Private WithEvents btnCreateUnboundFields As DevExpress.XtraEditors.SimpleButton
-		Private WithEvents btnRetrieveFields As DevExpress.XtraEditors.SimpleButton
-	End Class
-End Namespace
 
+        Private panelControl1 As DevExpress.XtraEditors.PanelControl
+
+        Private btnCreateUnboundFields As DevExpress.XtraEditors.SimpleButton
+
+        Private btnRetrieveFields As DevExpress.XtraEditors.SimpleButton
+    End Class
+End Namespace
